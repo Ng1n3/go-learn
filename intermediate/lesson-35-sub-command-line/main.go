b37a5fccee6095c80a8a322e4b141babc84ac367
Ng1n3 <code.with.muyiwa@gmail.com>
2025-11-21 21:53:34 +0000

    feat: subcommands with flag.NewFlagSet

---
+++ intermediate/lesson-35-sub-command-line/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	subcommand1 := flag.NewFlagSet("firstSub", flag.ExitOnError)
+	subcommand2 := flag.NewFlagSet("secondSub", flag.ExitOnError)
+
+	firstFlag := subcommand1.Bool("processing", false, "Command processing status")
+	secondFlag := subcommand1.Int("bytes", 1024, "Byte length of result")
+
+	flagsc2 := subcommand2.String("language", "Go", "Enter your language")
+
+	if len(os.Args) < 2 {
+		fmt.Println("This program requires additional arguments")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "firstSub":
+		subcommand1.Parse(os.Args[2:])
+		fmt.Println("subCommand1:")
+		fmt.Println("processing:", *firstFlag)
+		fmt.Println("bytes:", *secondFlag)
+
+	case "secondSub":
+		subcommand2.Parse(os.Args[2:])
+		fmt.Println("subcommand2:")
+		fmt.Println("language:", *flagsc2)
+
+	default:
+		fmt.Println("no subcomand entered!")
+		os.Exit(1)
+	}
+
+}
