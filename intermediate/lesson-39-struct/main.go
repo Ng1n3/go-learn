553ca783a2f9db498fd23eb9e89d8248870a4efe
Ng1n3 <code.with.muyiwa@gmail.com>
2025-11-23 18:34:42 +0000

    feat: Structs

---
+++ intermediate/lesson-39-struct/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type Person struct {
+	FirstName string `json:"first_name:" db:"firstn" xml:"first"`
+	//LastName  string `json:"last_name"`
+	LastName string `json:"last_name,omitempty"`
+	//Age      int    `json:"age"`
+	Age int `json:"-"`
+}
+
+func main() {
+	//person := Person{FirstName: "Jane", LastName: "", Age: 27}
+	person := Person{FirstName: "Jane", Age: 27}
+
+	jsonData, err := json.Marshal(person)
+	if err != nil {
+		log.Fatalln("Error marshalling struct: ", err)
+	}
+
+	fmt.Println(string(jsonData))
+
+	//fmt.Println(jsonData)
+}
